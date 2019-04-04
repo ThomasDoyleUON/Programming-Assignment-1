0515c91a6737d68925847cbb52ec2cd2acde743d
Thomas Doyle <thomas.doyle@uon.edu.au>
2019-04-04 05:14:52 +0000

    Testing the loop

--- Ciphers.c
+++ Ciphers.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+
 int main(void)
 {
-    printf("hello world\n");
+    char str[1000] = "qwertyuiopasdfghjklzxcvbnm"; //Inital String
+    char estr[1000]; //Encrypted String
+    int key = 2;
+    int i = 1;
+    
+    for(i = 0; str[i] != '\0'; ++i)
+        estr[i] = (str[i] + key)%26;
+        
+    printf("%s\n", estr);
 }
+
+
+
